6182c2b0ad47cf1ca9b78325eaf7f89ce976cc13
zhangkaizhuang <kenphp@yeah.net>
2023-06-01 19:53:30 +0800

    feat(webui): 新建web项目

---
+++ webui/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Watch TV',
+      meta: [
+        { name: 'description', content: 'Watch TV Web Site.' }
+      ],
+    }
+  },
+  ssr: false,
+  modules: ['@element-plus/nuxt'],
+})
