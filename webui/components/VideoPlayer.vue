5c0e0d9b8c439e8fed29e045a8e4be729e0fd66c
zhangkaizhuang <kenphp@yeah.net>
2023-06-02 18:59:37 +0800

    feat(webui): 添加video组件

---
+++ webui/components/VideoPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="video-player"></div>
+  </div>
+</template>
+<script setup>
+import 'mui-player/dist/mui-player.min.css'
+import MuiPlayer from 'mui-player'
+import Hls from 'hls.js'
+import { onMounted, onUnmounted, defineProps } from 'vue'
+
+const propos = defineProps({
+  title: String,
+  src: String,
+  poster: String,
+})
+
+let mp;
+
+onMounted(() => {
+  mp = new MuiPlayer({
+    container: "#video-player",
+    title: propos.title,
+    src: propos.src,
+    parse: {
+      type: "hls",
+      loader: Hls,
+      config: {}
+    }
+  });
+});
+onUnmounted(() => {
+  if (mp) {
+    mp.destory();
+  }
+});
+</script>
--- webui/pages/index.vue
+++ webui/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div>
+    <div>
+      <!-- https://cd-live-stream.news.cctvplus.com/live/smil:CHANNEL1.smil/playlist.m3u8 -->
+      <VideoPlayer title="测试" src="http://rzggis.com:8111/hls/57/index.m3u8" poster="" />
+    </div>
+  </div>
 </template>
 <script>
 definePageMeta({ layout: 'default' })
