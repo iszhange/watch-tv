189485b4cc0d0921a3eb7fdc32700a9a9ae9a0eb
kaizhuangzhang <kenphp@yeah.net>
2023-06-05 23:30:55 +0800

    feat(播放器): 国家选择组件

--- webui/components/VideoPlayer.vue
+++ webui/components/VideoPlayer.vue
@@ -3,33 +3,20 @@
   <div ref="headerbox" class="flex justify-center">
     <div v-if="curCountry" class="inline-block">
       <button type="button" class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-3 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
-      <span class="flex items-center">
-        <span class="fi fi-gr"></span>
-        <span class="ml-3 block truncate">{{ curCountry.name }}</span>
-      </span>
-    </button>
-
-    <ul v-if="countries" class="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" tabindex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
-      <!--
-        Select option, manage highlight styles based on mouseenter/mouseleave and keyboard navigation.
-
-        Highlighted: "bg-indigo-600 text-white", Not Highlighted: "text-gray-900"
-      -->
-      <li class="text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9" id="listbox-option-{{ index }}" role="option" @click="(index)=>{this.curCountry = countries[index];}" v-for="(item, index) in countries" >
-        <div class="flex items-center">
-          <img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-5 w-5 flex-shrink-0 rounded-full">
-          <!-- Selected: "font-semibold", Not Selected: "font-normal" -->
-          <span class="font-normal ml-3 block truncate">{{ item.name }}</span>
-        </div>
-        <span class="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4">
-          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-          </svg>
+        <span class="flex items-center">
+          <span :class="'fi fi-' + curCountry.code.toLowerCase()"></span>
+          <span class="ml-3 block truncate">{{ curCountry.name }}</span>
         </span>
-      </li>
+      </button>
 
-      <!-- More items... -->
-    </ul>
+      <ul v-if="countries" class="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" tabindex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
+        <li class="text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9" :id="'listbox-option-' + index" role="option" @click="(e)=>{this.curCountry = this.countries[Number(e.currentTarget.id.replace('listbox-option-', ''))]}" v-for="(item, index) in countries" >
+          <div class="flex items-center">
+            <span :class="'fi fi-' + item.code.toLowerCase()"></span>
+            <span class="font-normal ml-3 block truncate">{{ item.name }}</span>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -47,15 +34,12 @@ const propos = defineProps({
   countries: Array,
 })
 
-const headerbox = ref(null)
-const curCountry = ref(null);
-let mp;
+const headerbox = ref()
+let mp, curCountry
 
-watch(propos.countries, async (newVal) => {
-  if (newVal.length > 0) {
-    curCountry = newVal[0]
-  }
-})
+if (propos.countries.length > 0) {
+  curCountry = propos.countries[0]
+}
 
 onMounted(() => {
   mp = new MuiPlayer({
--- webui/pages/index.vue
+++ webui/pages/index.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <!-- https://cd-live-stream.news.cctvplus.com/live/smil:CHANNEL1.smil/playlist.m3u8 -->
-    <VideoPlayer title="测试" src="https://cd-live-stream.news.cctvplus.com/live/smil:CHANNEL1.smil/playlist.m3u8" poster="" />
+    <VideoPlayer title="测试" src="https://cd-live-stream.news.cctvplus.com/live/smil:CHANNEL1.smil/playlist.m3u8" poster="" :countries="countries" />
   </div>
 </template>
 <script setup>
 definePageMeta({ layout: 'default' })
 
 const appConfig = useAppConfig()
+let countries = ref()
 
-const countries = await $fetch(appConfig.apiHost+'/countries', {})
-console.log(111, countries)
+await $fetch('/countries', {
+  baseURL: appConfig.apiHost,
+}).then(res => {
+  if (res.code == 200) {
+    countries = res.data.list
+  }
+}).catch(err => {
+  console.log(err)
+})
+
 
 </script>
